2f691508c0966901bb1dca864aeef735a8b788e7
Jalendra <jalendra@JAS.(none)>
2013-03-10 14:40:18 +0530

    changed colro scheme

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -2,6 +2,6 @@ class User::SessionsController < Devise::SessionsController
 	private
 
 	def after_sign_in_path_for(resource)
-		user_dashboard_index_path(resource)
+		user_dashboard_index_path
 	end
 end
