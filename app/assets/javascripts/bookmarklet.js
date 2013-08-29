99b2a54ef4b318a5d4efd2b24eec31e6faa7b1d6
Jalendra Bhanarkar <jbmyid@gmail.com>
2013-08-29 16:38:16 +0530

    add tab drag and drop

--- app/assets/javascripts/bookmarklet.js
+++ app/assets/javascripts/bookmarklet.js
@@ -1,4 +1,3 @@
 url = window.document.documentURI;
 title = window.document.title;
-window.open("http://localhost:3000/user/tabs/new?title="+encodeURIComponent(title)+"&url="+encodeURIComponent(url),"Import Tab", "resizable=1,width=350,height=250");
-// alert("hello")
+window.open("http://gorss.jbmyid.in/user/tabs/new?title="+encodeURIComponent(title)+"&url="+encodeURIComponent(url),"Import Tab", "resizable=1,width=350,height=250");
