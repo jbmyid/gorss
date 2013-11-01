e83de3e1efcb74c8d2e90489d09c3514a08c6021
Jalendra Bhanarkar <jbmyid@gmail.com>
2013-11-02 00:44:24 +0530

    fix user color

--- Gemfile
+++ Gemfile
@@ -43,4 +43,4 @@ gem 'omniauth-facebook'
 # gem 'capistrano'
 
 # To use debugger
-gem 'debugger'
+# gem 'debugger'
--- app/models/person.rb
+++ app/models/person.rb
@@ -8,4 +8,9 @@ class Person < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,:provider, :uid
   # attr_accessible :title, :body
+  ["Admin","User"].each do |m|
+    define_method m.downcase+"?" do
+      self.type==m
+    end
+  end
 end
